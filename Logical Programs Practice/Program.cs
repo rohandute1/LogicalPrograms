7abff2521855b11c1e3f8c4207a8061654704765
Rohan <rohandute1@gmail.com>
2023-05-23 21:47:38 +0530

    [Rohan] Adding welcome message

---
+++ Logical Programs Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical programs");
+            Console.ReadLine();
+        }
+    }
+}
