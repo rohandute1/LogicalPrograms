14dc08f46360aecfdf8cd7032bc250edaf341d4e
Rohan <rohandute1@gmail.com>
2023-05-24 00:10:48 +0530

    [Checking perfect number]

---
+++ Logical Programs Practice/PerfectNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs_Practice
+{
+    internal class PerfectNumber
+    {
+        public static void CheckingPerfectNumber(int n)
+        {
+
+            bool isPerfect = IsPerfectNumber(n);
+
+            if (isPerfect) 
+            {
+                Console.WriteLine($"It is a prfect numnber", + n); 
+            }
+            else 
+            {
+                Console.WriteLine($"It is not a prfect numnber", +n);
+            }
+            Console.ReadLine();
+        }
+        public static bool IsPerfectNumber(int number) 
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++) 
+            {
+                if (number % i == 0) 
+                {
+                    sum += i;
+                }
+            }
+            return sum == number;
+        }    
+
+    }
+}
--- Logical Programs Practice/Program.cs
+++ Logical Programs Practice/Program.cs
@@ -11,10 +11,28 @@ namespace Logical_Programs_Practice
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical programs");
-            Console.Write("Please enter the number: ");
-            int N = Convert.ToInt32(Console.ReadLine());
-            FibonacciSeries fibonacci = new FibonacciSeries();
-            FibonacciSeries.PrintFibonacciSeries(N);
+            Console.WriteLine("Please chose the option given below: ");
+            Console.WriteLine(" 1.Fibonacci series\n 2.Perfect number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.Write("Please enter the number: ");
+                    int N = Convert.ToInt32(Console.ReadLine());
+                    FibonacciSeries fibonacci = new FibonacciSeries();
+                    FibonacciSeries.PrintFibonacciSeries(N);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Please enter the number: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    PerfectNumber perfect = new PerfectNumber();
+                    PerfectNumber.CheckingPerfectNumber(n);
+                    break;
+            }
+               
+
+            
             Console.ReadLine();
         }
     }
