b8383cad3241155e0eb7f9d776c07a372017cd42
Rohan <rohandute1@gmail.com>
2023-05-24 15:03:55 +0530

    [Rohan] Checking prime number or not

---
+++ Logical Programs Practice/PrimeNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs_Practice
+{
+    internal class PrimeNumber
+    {
+        public static void PrimeNumberChecker(int number) 
+        {
+            
+
+            bool isPrime = IsPrimeNumber(number);
+
+            if (isPrime) 
+            {
+                Console.WriteLine($"{number} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a prime number.");
+            }
+            Console.ReadLine();
+        }
+        public static bool IsPrimeNumber( int number )
+        {
+            if ( number <= 1 )
+            {
+                return false;
+            }
+            if ( number == 2 ) 
+            {
+                return true;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- Logical Programs Practice/Program.cs
+++ Logical Programs Practice/Program.cs
@@ -12,22 +12,26 @@ namespace Logical_Programs_Practice
         {
             Console.WriteLine("Welcome to Logical programs");
             Console.WriteLine("Please chose the option given below: ");
-            Console.WriteLine(" 1.Fibonacci series\n 2.Perfect number");
+            Console.WriteLine(" 1.Fibonacci series\n 2.Perfect number\n 3.Prime number checker");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     Console.Write("Please enter the number: ");
                     int N = Convert.ToInt32(Console.ReadLine());
-                    FibonacciSeries fibonacci = new FibonacciSeries();
                     FibonacciSeries.PrintFibonacciSeries(N);
                     break;
 
                 case 2:
                     Console.WriteLine("Please enter the number: ");
                     int n = Convert.ToInt32(Console.ReadLine());
-                    PerfectNumber perfect = new PerfectNumber();
                     PerfectNumber.CheckingPerfectNumber(n);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter a number: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    PrimeNumber.PrimeNumberChecker(number);
+
                     break;
             }
                
